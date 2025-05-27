a51f783f91af2fb20e43c7d310b731a53aaf81d6
Radist <radist@rubium.ru>
2025-05-27 09:07:20 +0300

    fix codestile

--- src/Enum/CategoryEnum.php
+++ src/Enum/CategoryEnum.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace PlaystationStoreApi\Enum;
 
-use PlaystationStoreApi\Enum\EnumFromName;
-
 enum CategoryEnum: string
 {
     use EnumFromName;
--- src/Enum/EnumFromName.php
+++ src/Enum/EnumFromName.php
@@ -10,4 +10,4 @@ trait EnumFromName
     {
         return constant("self::$name");
     }
-} 
+}
